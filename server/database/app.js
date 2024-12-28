e65eee7cffd934bdc271bfda3c81280fc6d44ee9
“woahgrommit” <02-crummy-trains@icloud.com>
2024-12-28 18:51:23 -0500

    updated app.js

--- server/database/app.js
+++ server/database/app.js
@@ -59,16 +59,37 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+  try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+  try {
+    const dealerships = await Dealerships.find({state: req.params.state});
+    res.json(dealerships);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealerships' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+  try {
+    const dealership = await Dealerships.findOne({id: req.params.id});
+    if (!dealership) {
+      return res.status(404).json({ error: 'Error: Dealership not found' });
+    }
+    res.json(dealership);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealership' });
+  }
 });
 
 //Express route to insert review
