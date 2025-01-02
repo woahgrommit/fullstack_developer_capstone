6625670368275ce9228caba579506fac99cc6f9f
“woahgrommit” <02-crummy-trains@icloud.com>
2025-01-02 16:18:42 -0500

    corrections

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,14 +1,9 @@
 # Uncomment the required imports before adding the code
-from .models import CarMake, CarModel
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
+from .models import CarMake, CarModel
 from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
