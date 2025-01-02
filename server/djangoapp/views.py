4a71644de9ae112b0104720e55cf08180b931730
“woahgrommit” <02-crummy-trains@icloud.com>
2025-01-02 18:34:39 -0500

    Corrections

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -54,9 +54,9 @@ def registration(request):
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    if not username_exist:
+    except Exception as e:
         # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
+        logger.debug(f"{e}, {} is new user".format(username))
 
     # If it is a new user
     if not username_exist:
