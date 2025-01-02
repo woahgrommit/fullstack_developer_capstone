b0b1a31e126665885366cf2e2994ff643fbf2f38
“woahgrommit” <02-crummy-trains@icloud.com>
2025-01-02 18:44:24 -0500

    Corrections

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -40,7 +40,7 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
+    context = {} # noqa: E128
 
     data = json.loads(request.body)
     username = data["userName"]
@@ -49,7 +49,7 @@ def registration(request):
     last_name = data["lastName"]
     email = data["email"]
     username_exist = False
-    email_exist = False
+    email_exist = False # noqa: E128
     try:
         # Check if user already exists
         User.objects.get(username=username)
@@ -85,7 +85,9 @@ def get_cars(request):
     car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({
+            "CarModel": car_model.name, 
+            "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -124,12 +126,15 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if request.user.is_anonymous == False:
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            response = post_review(data) # noqa: E128
             return JsonResponse({"status": 200})
-        except Exception as err:
-            return JsonResponse({"status": 401, "message": f"{err=} Error in posting review"})
+        except Exception as e:
+            return JsonResponse({
+                "status": 401, 
+                "message": f"{e} Error in posting review"
+                })
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
